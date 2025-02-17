66fc43e39b8ad7d9d14058d4a58151425d01b53e
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-02-17 10:46:59 +0530

    added a new project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 import os
 
 app = Flask(__name__, template_folder='.', static_folder='static')
@@ -6,9 +6,21 @@ app = Flask(__name__, template_folder='.', static_folder='static')
 # Ensure the static/images directory exists
 os.makedirs(os.path.join(app.static_folder, 'images'), exist_ok=True)
 
+@app.route('/linkedin')
+def linkedin():
+    return redirect('https://www.linkedin.com/in/malapaka-krishna-sairam')
+
+@app.route('/github')
+def github():
+    return redirect('https://github.com/krishnasairammalapaka')
+
 @app.route('/traffic-management')
 def traffic_management():
     return render_template('traffic-management.html')
+
+@app.route('/greens-cart')
+def greens_cart():
+    return render_template('greens-cart.html')
 
 @app.route('/')
 def home():
